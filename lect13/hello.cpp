7b5bdd971ff50ad6e14e543fe9bab240df6960bf
Jaya Karwani <jayakarwani4@gmail.com>
2025-12-20 23:10:21 +0530

    binary exponentiation

--- lect13/hello.cpp
+++ lect13/hello.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 // buy and sell stock
+// binary exponentiation 
+double myPow(double x,int n){
+    long binForm=n;
+    double ans=1;
+    while(binForm>0){
+        if(binForm%2==1){
+            ans*=x;
+        }
+        x*=x;
+        binForm /=2;
+    }
+    return ans;
+}
 int main(){
-      
+    int n=10;
+    double x=2.0;
+    cout<< myPow(x,n);
     
 }
