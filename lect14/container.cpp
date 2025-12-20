3e94940e1f3297553702452c76a79f0fe4084fb6
Jaya Karwani <jayakarwani4@gmail.com>
2025-12-21 02:26:06 +0530

    container with most water

---
+++ lect14/container.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+//brute force approach
+int mostWater(int height[9],int size){
+    int maxWater=0,h,w;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            int width=j-i;
+            int hei=min(height[i],height[j]);
+            int ans=hei*width;
+            if(ans>maxWater){
+                maxWater=ans;
+            }
+        }
+    }
+    
+    return maxWater;
+}
+
+int main(){
+    int height[9]={1,8,6,2,5,4,8,3,7};
+    int size=9;
+    cout<< mostWater(height,size);
+}
