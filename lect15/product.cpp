91045bf811bfab8101ad29acfdb56fd4d7c20a38
Jaya Karwani <jayakarwani4@gmail.com>
2025-12-23 23:37:22 +0530

    product of array except self

--- lect15/product.cpp
+++ lect15/product.cpp
@@ -3,6 +3,30 @@
 #include<algorithm>
 #include<iostream>
 using namespace std;
+//product of array except self 
+// we cannot use division operator
+//238 on leetcode
+// brute force approach
+
+// int main(){
+//     vector<int> nums={1,2,3,4};
+//     int n=nums.size();
+//     vector<int> ans(n);
+//      for(int i=0;i<n;i++){
+//         int product=1;
+//         for(int j=0;j<n;j++){
+//             if(i!=j){
+//                 product*=nums[j];
+//             }
+//         }
+//         ans[i]=product;
+//      }
+//      for(int i=0;i<n;i++){
+//      cout<<ans[i] <<" ";
+//      }
+// }
+
+// optimal approach
 
 int main(){
     
