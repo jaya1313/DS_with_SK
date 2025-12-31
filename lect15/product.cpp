2974f05fa71078427a3f0a8c4e0302cf950d7d70
Jaya Karwani <jayakarwani4@gmail.com>
2025-12-31 18:17:59 +0530

    array product except self

--- lect15/product.cpp
+++ lect15/product.cpp
@@ -28,6 +28,43 @@ using namespace std;
 
 // optimal approach
 
-int main(){
+// int productExceptSelf(vector<int>nums,int size){
+//     vector<int> prefix;
+//     vector<int> suffix;
+//     vector<int> ans;
+
+//     for(int i=1;i<size;i++){
+//         prefix[0]=1;
+//         prefix[i]=prefix[i-1]*nums[i-1];
+//     }
+//     for(int i=size-2;i>=0;i--){
+//         suffix[size-1]=1;
+//         suffix[i]=suffix[i+1]*nums[i+1];
+//     }
+//     for(int i=0;i<size;i++){
+//        ans[i]=prefix[i]*suffix[i];
+//     }
+//     return ans;
     
+// }
+
+//to enhance the space complexity, below code is there, the logic is same as the above.
+
+int productExceptSelf(vector<int>nums,int size){
+    vector <int> ans(size);
+    for(int i=1;i<size;i++){
+        ans[i]=ans[i-1]*nums[i-1];
+    }
+    int suffix=1;
+    for(int i=size-2;i>=0;i--){
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
+    }
+    return ans[4];
+}
+
+int main(){
+    vector<int>nums={1,2,3,4};
+    int size=nums.size();
+    cout << productExceptSelf(nums, size);
 }
