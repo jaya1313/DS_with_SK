2824cf135d2081e9147fb023a4729c1bf5cabd81
Jaya Karwani <jayakarwani4@gmail.com>
2026-01-29 21:38:02 +0530

    boolAllocation

--- lect21/bookAllocation.cpp
+++ lect21/bookAllocation.cpp
@@ -2,6 +2,51 @@
 #include<vector>
 using namespace std;
 
+bool isValid(vector<int>arr,int n,int m,int maxAllowedPages){ //O(n)
+    int stu=1, pages=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxAllowedPages){
+            return false;
+        }
+        if(pages+arr[i]<=maxAllowedPages){
+            pages+=arr[i];
+        }
+        else{
+            stu++;
+            pages=arr[i];
+        }
+    }
+    return stu>m ?false:true;
+}
+
+int allocateBooks(vector<int>arr,int n,int m){
+    if(m>n){
+        return -1;
+    }
+    int st=0,ans=-1;
+    int sum=0;
+    for(int i=0;i<n;i++){ //O(n)
+        sum+=arr[i];
+    }
+    int end= sum;
+    while(st<=end){   //O(logN *n)
+        int mid=st+(end-st)/2;
+        if(isValid(arr,n,m,mid)){
+            ans=mid;
+          end=mid-1;
+        }
+        else{
+           end=mid+1;
+        }
+    }
+   return ans;
+}
+
 int main(){
+   vector<int> arr={2,1,3,4};
+   int n=4;
+   int m=2;
+   cout<< allocateBooks(arr,n,m)<<endl;
+   return 0;
     
 }
