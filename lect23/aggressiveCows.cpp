7e786ef0d6b5746fe8a04452a7e0af7a7c38b991
Jaya Karwani <jayakarwani4@gmail.com>
2026-01-30 23:17:35 +0530

    Aggressive cows

---
+++ lect23/aggressiveCows.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+
+bool isPossible(vector <int> arr,int n,int c,int maxMinLength){
+
+}
+
+int maxMinDistance(vector<int>arr,int n,int c){
+    int st=1,end=arr[1]-arr[n],ans=-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(isPossible(arr,n,c,mid)){
+            ans=mid;
+            st=mid+1;
+        }else{
+            end=mid -1;
+        }
+    }
+}
+int main(){
+    vector <int> arr={1,2,8,4,9};
+    int n=5;  // no.of stalls
+    int c=3; //no.of cows
+    cout<< maxMinDistance(arr,n,c) << endl;
+    return 0;
+
+}
