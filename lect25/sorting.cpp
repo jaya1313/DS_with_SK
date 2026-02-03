d90662319b6fdea4ffe4ea3ce6eefb88e0a9bf74
Jaya Karwani <jayakarwani4@gmail.com>
2026-02-03 14:03:55 +0530

    sorting array with 0s,1s,2s

--- lect25/sorting.cpp
+++ lect25/sorting.cpp
@@ -2,8 +2,46 @@
 #include<vector>
 using namespace std;
 
-//Sorting an array with 0s,1s and 2s.
+//Sorting an array with 0s,1s and 2s.  Or sort colors(leetcode)
+
+void sortColors(vector<int>&arr,int n){
+    int count0=0,count1=0,count2=0;
+
+     for(int i=0;i<n;i++){
+     if(arr[i]==0)
+     count0++;
+     else if(arr[i]==1)
+     count1++;
+     else
+     count2++;
+}
+    int idx=0;
+    for(int i=0;i<count0;i++){
+        arr[idx++]=0;
+    }
+    for(int i=0;i<count1;i++){
+        arr[idx++]=1;
+    }
+    for(int i=0;i<count2;i++){
+        arr[idx++]=2;
+    }
+}
+
+void sortColors2(vector<int> &arr,int n){    //more optimised way,DNF(dutch national flag approach)
+
+}
+
+void printArray(vector<int>arr,int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 int main(){
-
+    vector<int>arr={2,0,1,0,2,1,0,1,2,0};
+    int n=arr.size();
+    sortColors(arr,n);
+    printArray(arr,n);
+    return 0;
 }
