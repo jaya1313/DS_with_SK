67a3a771fbc46e85821e99fc2966708801c0f3fe
Jaya Karwani <jayakarwani4@gmail.com>
2026-02-21 13:07:06 +0530

    vector in STL

---
+++ lect27/vector.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ // c++ STL
+ // vector (1st container)
+
+ int main(){
+    vector<int>vec={1,2,3,4,5};
+    vec.push_back(8);  //added to the last
+    vec.pop_back();    // delete from the last  // does not affect the capacity
+    vec.emplace_back(9);  // same as push_back
+    vec.erase(vec.begin()); // delete 1st element
+    vec.erase(vec.begin()+2); // delete 3rd element (index 2)
+    vec.insert(vec.begin(),1);  // add 1 at 0 index
+    vec.insert(vec.begin()+2,4);  // add 4 at 2 index
+    //vec.clear();      // clears a vector
+
+    for(int val : vec){
+        cout<< val <<" ";
+    }
+    cout<<endl;
+    cout<< vec.size() << endl;
+    cout << vec.capacity() << endl;
+    cout<<"is empty:" << vec.empty() << endl; // checks if vec is empty or not,if true return 1 otherwise 0
+    // cout<< vec[3] << endl;  // or cout<< vec.at(3)
+    // cout<< vec.front()<<endl;    //returns the 1st element 
+    // cout << vec.back() << endl;  //returns the last element
+
+
+ }
