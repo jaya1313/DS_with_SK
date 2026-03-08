d7eb054289d28bc56a5c0d25e1c217b83ff68bc1
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-08 17:42:02 +0530

    STL

--- lect27/map.cpp
+++ lect27/map.cpp
@@ -6,28 +6,47 @@ using namespace std;
 //it has key value pair
 
 int main(){
-    map<string,int> m;
+    // map<string,int> m;
 
-    m["tv"]=100;
-    m["laptop"]=100;
-    m["headphones"]=50;
-    m["tablet"]=120;
-    m["mobile"]=50;
-    m["watch"]=100;
+    // m["tv"]=100;
+    // m["laptop"]=100;
+    // m["headphones"]=50;
+    // m["tablet"]=120;
+    // m["mobile"]=50;
+    // m["watch"]=100;
 
-    m.emplace("camera",25);
+    // m.emplace("camera",25);
 
-    m.erase("tv");
+    // m.erase("tv");
+
+    // for(auto p: m){
+    //     cout << p.first << " " <<p.second<< endl;
+    // }
+
+    // if(m.find("camera") != m.end()) {
+    //     cout << "found\n";
+    // }
+    // else{
+    //     cout <<"not found";
+    // }
+
+
+    multimap<string, int> m;
+
+    m.emplace("tv",100);
+    m.emplace("tv",100);
+    m.emplace("tv",100);
+    m.emplace("tv",100);
+
+    m.erase(m.find("tv"));
 
     for(auto p: m){
-        cout << p.first << " " <<p.second<< endl;
-    }
+         cout << p.first << " " <<p.second<< endl;
+     }
 
-    if(m.find("camera") != m.end()) {
-        cout << "found\n";
-    }
-    else{
-        cout <<"not found";
-    }
+     // one more type of map
+
+     // unordered map
+     //unordered_map<string,int>m;
     return 0;
 }
