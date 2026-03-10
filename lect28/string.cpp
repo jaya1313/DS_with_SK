b773fd29a5ba6f1fd46e30335232c33c0e93489d
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-10 23:54:59 +0530

    string

--- lect28/string.cpp
+++ lect28/string.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
-#include<string>
+#include<cstring>
 
 using namespace std;
 
+//strings 
+
 int main(){
+
+    //character strings
+
+    char str[]="college";      // string literals
+    char str2[]={'a','b','c','\0'};
+    cout << str << endl;
+    cout << str2 << endl;
+
+    cout << strlen(str) << endl;
+    cout << strlen(str2) << endl;
+
+    // input of char string
+
+    char str1[100];
     
+    cout << "Enter char Array:";
+    //cin >> str1;
+
+    //getline input (cin.getline(str,len,demir means any symbol to stop getting input))
+
+    cin.getline(str1,12,'@');
+
+    cout << "output:" << str1 << endl;
+    return 0;
 }
