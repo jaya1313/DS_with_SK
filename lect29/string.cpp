d4c889d27ccf1ee0fb0a263d9374b47f92b31ae2
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-15 20:44:53 +0530

    string questions

--- lect29/string.cpp
+++ lect29/string.cpp
@@ -7,7 +7,37 @@ using namespace std;
 
 //valid palindrome
 
+bool isalphaNum(char ch){
+    if(ch>='0' && ch<='9' || (tolower(ch)>= 'a' && tolower(ch)<='z')){
+        return true;
+    }
+    return false;
+}
+
+bool validPalindrome(string s,int n){
+    int st=0;
+    int end=n-1;
+    while(st<end){
+        if(!isalphaNum(s[st])){
+            st++;
+        }
+        if(!isalphaNum(s[end])){
+            end--;
+        }
+        if(tolower(s[st])!= tolower(s[end])){
+            return false;
+        }
+        st++;
+        end--;
+    }
+    return true;
+}
+
 
 int main(){
+    string s="apna";
+    int n= s.length();
+    cout << validPalindrome(s,n);   // returns 0 or 1
 
+    
 }
