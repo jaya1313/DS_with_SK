6f5bf4caa22257057ec4e7660674467cb135cbe1
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-16 21:57:26 +0530

    string questions

--- lect29/removeOccurrence.cpp
+++ lect29/removeOccurrence.cpp
@@ -5,7 +5,23 @@
 
 using namespace std;
 
+//1910 on leetcode
+//s.find() function gives the first index of first occurrence of part
+//s.erase() function delete the elements from main string
+//like we pass s.erase(1,4) means it erases the element at index 1 to 4 no. of char ,from index 1 to 4.
+
+string removeAllOccurrence(string str,string part){
+    while(str.length() >0 && str.find(part) < str.length()){
+
+        str.erase(str.find(part),part.length());
+    }
+    return str;
+}
+
 int main(){
 
-    
+    string str="daabcbaabcbc";
+    string part="abc";
+    cout << removeAllOccurrence(str,part);
+
 }
