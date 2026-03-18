81e15594fbf6bef10cfbc6df8c4a17b1f2b5bccd
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-18 21:56:35 +0530

    reverse words  in string

--- lect31/reverseWords.cpp
+++ lect31/reverseWords.cpp
@@ -1,13 +1,34 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
 //reverse the words in a string
 //151 on leetcode
 
+string reverseWords(string s,int n){
 
+      string ans=" ";
+
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++){
+        string word=" ";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.length() >0){
+            ans+=" "+word;
+        }
+    }
+    return ans.substr(1);          // delete only beggining space(before first word)
+
+}
 
 int main(){
-
+    string s="hello world";
+    int n=s.length();
+    cout << reverseWords(s,n);
 }
