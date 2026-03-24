7b4a8870971fd95a37de9ac6917863cc11fd4c43
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-24 21:09:41 +0530

    Sieve of Eratosthenes

--- lect33/math.cpp
+++ lect33/math.cpp
@@ -2,6 +2,24 @@
 #include<string>
 #include<vector>
 
+using namespace std;
+
+// math in c++
+// 1. check prime or not
+//2.count prime numbers between a given range(brute force approach)
+// optimised approachfor counting prime no.s(Sieve of Eratosthenes)
+
+string isPrime(int n){
+      for(int i=2;i<n;i++){
+        if(n%i==0){
+            return "non prime";
+        }
+      }
+      return "prime";
+}
+
 int main(){
+    int n=47;
+    cout << isPrime(n);
     
 }
