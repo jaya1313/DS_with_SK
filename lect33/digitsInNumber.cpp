9b4d5f95862e2c14be5562bd34518b443a6e9e94
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-25 00:44:24 +0530

    math

--- lect33/digitsInNumber.cpp
+++ lect33/digitsInNumber.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
+#include<math.h>
 
 using namespace std;
 
@@ -16,6 +18,31 @@ void sumDigits(int n){
     cout << sum << endl;
 }
 
+// amstrong no.--- a number that is equal to the sum of cubes of its digit
+
+bool isAmstrong(int n1){
+    int copyN=n1;
+    int sumOfCubes=0;
+
+    while(n1!=0){
+        int dig=n1%10;
+        sumOfCubes+=(dig*dig*dig);
+        n1=n1/10;
+    }
+   return sumOfCubes==copyN;
+}
+
 int main(){
-    sumDigits(6538);
+    int n=6538;
+    sumDigits(n);
+    cout << (int)(log10(n)+1) << endl;   // it returns the count of digits(shortcut method)
+
+    int n1=153;
+    if(isAmstrong(n1)){
+        cout << "is an amstrong number";
+    }
+    else{
+        cout << "not an amstrong number";
+    }
+    return 0;
 }
