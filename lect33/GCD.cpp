d76174da9a914ac1332d12491472671c49fa9f58
Jaya Karwani <jayakarwani4@gmail.com>
2026-03-25 22:53:32 +0530

    math

--- lect33/GCD.cpp
+++ lect33/GCD.cpp
@@ -24,9 +24,31 @@ int findGCD(int a, int b){
 // eg- gcd(20,28)-> gcd(20,8)-> gcd(12,8)-> gcd(4,8)-> gcd(4,4)-> gcd(0,4)
 //hence if 1 no. becomes 0 then opp no. is the gcd hence gcd is 4
 
+// now a more optimised way of doing this ,means instead of substracting we can do modulus
+// for gcd(a,b)
+// gcd(a%b,b)  if a>b
+//gcd(a,b%a)   if b>a and is known as euclids algorithm
+
+//code
+int gcd(int a,int b){
+    while(a>0 && b>0){
+        if(a>b){
+            a= a % b;
+        }
+        else{
+            b = b % a;
+        }
+    }
+    if(a==0)
+    return b;
+    else
+    return a;
+}
+
 int main(){
     int a=20;
     int b=28;
-    cout << findGCD(a,b);
+    cout << findGCD(a,b) << endl;
+    cout << gcd(a,b) << endl;
     return 0;
 }
