b81d440ed140703242dfd0ab1f8eba969e919b98
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-02 00:44:52 +0530

    2 Sum problem

--- lect37/2Sum.cpp
+++ lect37/2Sum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
@@ -36,12 +37,30 @@ using namespace std;
 //     }
 // }
 
-//optimised approach  (using hashing)
+//optimised approach  (using hashing,unordered map)
 
+vector<int>twoSum(vector<int>& arr,int tar){
+    unordered_map<int,int> m;
+    vector<int> ans;
 
-
-
+    for(int i=0;i<arr.size();i++){
+         int first= arr[i];
+         int sec=tar-first;
+         if(m.find(sec) != m.end()){
+            ans.push_back(i);
+            ans.push_back(m[sec]);
+            break;
+         }
+         m[first] =i;
+    }
+    return ans;
+}
 
 int main(){
-
+    vector<int> arr={5,2,11,7,15};
+    int target=9;
+    vector<int> result=twoSum(arr,target);
+    for(int x:result){
+        cout << x <<" ";
+    }
 }
