40c472fa498162d8e07af8a085a8f7a87085b7e9
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-03 00:01:02 +0530

    2 Sum problem

--- lect37/missingAndRpeating.cpp
+++ lect37/missingAndRpeating.cpp
@@ -14,16 +14,23 @@ vector<int> findMissingAndRepeatingValues(vector<vector<int>>grid){
     unordered_set<int> s;
     int a,b;
 
+    int expSum=0, actualSum=0;
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
+            actualSum+=grid[i][j];
             if(s.find(grid[i][j]) != s.end()){
                 a=grid[i][j];
                 ans.push_back(a);
-                break;
             }
             s.insert(grid[i][j]);
         }
-    }
+    } 
+    expSum= (n*n) *(n*n +1)/2;
+    b=expSum + a - actualSum;
+    ans.push_back(b);
+    
+    return ans;
 }
 
 int main(){
