2fa6b270973b0ccee5b582dad2ca1bdd5088241e
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-05 22:44:41 +0530

    3Sum problem

--- lect38/ThreeSum.cpp
+++ lect38/ThreeSum.cpp
@@ -1,10 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<set>
 using namespace std;
 
  // 3 sum problem
+
+//  Given an integer array nums, return all the triplets [nums[i], nums[j],
+// nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
 // 15 on leetcode
 
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        int n= nums.size();
+        vector<vector<int>> ans;
+
+        set<vector<int>> s;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    if(nums[i] + nums[j] +nums[k] == 0){
+                          vector<int> trip = {nums[i] , nums[j] , nums[k]};
+                          sort(trip.begin(), trip.end());
+
+                          if(s.find(trip) == s.end()){
+                          s.insert(trip);
+                          ans.push_back(trip);
+
+                          }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 
 
 int main(){
