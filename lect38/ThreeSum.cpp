0cbe17e89503dcad00eb95f2108ac40538868198
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-07 21:35:21 +0530

    3Sum problem

--- lect38/ThreeSum.cpp
+++ lect38/ThreeSum.cpp
@@ -13,30 +13,30 @@ using namespace std;
 // 15 on leetcode
 
 // brute force approach
-vector<vector<int>> threeSum(vector<int>& nums) {
-        int n= nums.size();
-        vector<vector<int>> ans;
+// vector<vector<int>> threeSum(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<vector<int>> ans;
 
-        set<vector<int>> s;
+//         set<vector<int>> s;
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(nums[i] + nums[j] +nums[k] == 0){
-                          vector<int> trip = {nums[i] , nums[j] , nums[k]};
-                          sort(trip.begin(), trip.end());
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 for(int k=j+1; k<n; k++){
+//                     if(nums[i] + nums[j] +nums[k] == 0){
+//                           vector<int> trip = {nums[i] , nums[j] , nums[k]};
+//                           sort(trip.begin(), trip.end());
 
-                          if(s.find(trip) == s.end()){
-                          s.insert(trip);
-                          ans.push_back(trip);
+//                           if(s.find(trip) == s.end()){
+//                           s.insert(trip);
+//                           ans.push_back(trip);
 
-                          }
-                    }
-                }
-            }
-        }
-        return ans;
-    }
+//                           }
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
 
     // better approach
     
