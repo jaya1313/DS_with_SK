b113fff0966b5af79a2fc9d8bc9135335d572a0c
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-10 12:26:49 +0530

    3sum problem

--- lect38/ThreeSum.cpp
+++ lect38/ThreeSum.cpp
@@ -39,13 +39,36 @@ using namespace std;
 //     }
 
     // better approach
+    // using hashing
+    // vector<vector<int>> threeSum(vector<int>& nums) {
+    //      int n= nums.size();
+    //      vector<vector<int>> ans;
+    //         set<vector<int>> uniqueTriplets;
+    //         for(int i=0;i<n;i++){
+    //             int tar= - nums[i];
+    //             set<int> s;
+    //             for(int j=i+1;j<n;j++){
+    //                 int third= tar-nums[j];
+    //                 if(s.find(third) != s.end()){
+    //                      vector<int>trip={nums[i],nums[j],third};
+    //                      sort(trip.begin(),trip.end());
+    //                     uniqueTriplets.insert(trip);
+    //                 }
+    //                 s.insert(nums[j]);
+    //             }
+    //         }
+    //         return ans;
+    // }
+    
+
+    //optimal/best approach
     //using two pointer approach
     // sort the array first
-    // using hashing
     vector<vector<int>> threeSum(vector<int>& nums) {
-         int n= nums.size();
+        int n= nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
     }
-    
 
 
 int main(){
