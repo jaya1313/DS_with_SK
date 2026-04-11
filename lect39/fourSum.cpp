7d391ceff29674bffeec859d594fe974215c3c59
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-11 23:58:01 +0530

    4sum problem

--- lect39/fourSum.cpp
+++ lect39/fourSum.cpp
@@ -5,3 +5,10 @@
 using namespace std;
 
  // 4 sum problem
+
+//Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+//0 <= a, b, c, d < n
+//a, b, c, and d are distinct.
+//nums[a] + nums[b] + nums[c] + nums[d] == target
+//You may return the answer in any order.
