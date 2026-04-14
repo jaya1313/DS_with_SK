4e007000e1f8ea8f31afd4475e770a2ce6c70da3
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-14 17:03:47 +0530

    subarray sum equals to k

--- lect40/subarraySum.cpp
+++ lect40/subarraySum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 #include<algorithm>
 
 using namespace std;
@@ -28,7 +29,28 @@ int subarraySum(vector<int>& nums, int k){
     return count;
 }
 
-int subarraySum2(vector<int>& nums, int k){
+int subarraySum2(vector<int>& arr, int k) {
+        int n=arr.size();
+        int count = 0;
+        vector<int> prefixSum(n,0);
+        prefixSum[0] = arr[0];
+        for(int i=1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+        unordered_map<int,int> m;
+        for(int j=0; j<n; j++){
+            if(prefixSum[j] == k) count++;
+
+            int val = prefixSum[j] - k;
+            if(m.find(val) != m.end()) {
+                count+= m[val];
+            }
+            if(m.find(prefixSum[j]) == m.end()) {
+                m[prefixSum[j]] == 0;
+            }
+             m[prefixSum[j]]++;
+        }
+        return count;
     
 
 }
@@ -36,6 +58,8 @@ int subarraySum2(vector<int>& nums, int k){
 int main(){
     vector<int> nums={9,4,20,3,10,5,3};
     int k=33;
-    cout << subarraySum(nums, k) << endl;
+    vector<int> arr={9,4,0,20,3,10,5,3};
+    //cout << subarraySum(nums, k) << endl;
+    cout << subarraySum2(arr, k) << endl;
         return 0;
 }
