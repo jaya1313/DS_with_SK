12a721c84619360f47e118931a157747dffa4c51
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-21 10:42:56 +0530

    recursion

--- lect41/recursion.cpp
+++ lect41/recursion.cpp
@@ -1,1 +1,18 @@
 //recursion
+//backtracking is dine in recursion
+
+#include<iostream>
+using namespace std;
+
+
+//print numbers using recursion
+void printNums(int n) {
+    if(n==1){       //base case
+        cout << "1\n";
+        return;
+    }
+
+    cout << n <<" ";
+    printNums(n-1);
+
+}
