fe53d19366f689ab14316f3b4829d1d4915e4779
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-22 10:33:11 +0530

    recursion

--- lect41/recursion.cpp
+++ lect41/recursion.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 
 //print numbers using recursion
-// void printNums(int n) {
-//     if(n==1){       //base case
-//         cout << "1\n";
-//         return;
-//     }
+void printNums(int n) {
+    if(n==1){       //base case
+        cout << "1\n";
+        return;
+    }
 
-//     cout << n <<" ";
-//     printNums(n-1);
+    cout << n <<" ";
+    printNums(n-1);
 
-// }
+}
 
 //factorial using recursion
 int factorial(int n){
@@ -25,9 +25,18 @@ int factorial(int n){
     return n*factorial(n-1);
 }
 
+//sum of n numbers
+int sum(int n){
+    if(n==1){                       // if(n==0)  return 0  this can also be a condition
+        return 1;
+    }
+    return n + sum(n-1);
+}
 
 
 int main(){
-    //printNums(5);
-    cout << factorial(5);
+    printNums(5);
+    cout << factorial(5) << endl;
+    cout << sum(5);
+    return 0;
 }
