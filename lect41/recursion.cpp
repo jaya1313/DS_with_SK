2f26f31b69e1d2f79348b93e5efef8b30d56dd87
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-22 10:10:24 +0530

    recursion

--- lect41/recursion.cpp
+++ lect41/recursion.cpp
@@ -1,18 +1,33 @@
 //recursion
-//backtracking is dine in recursion
+//backtracking is done in recursion
 
 #include<iostream>
 using namespace std;
 
 
 //print numbers using recursion
-void printNums(int n) {
-    if(n==1){       //base case
-        cout << "1\n";
-        return;
+// void printNums(int n) {
+//     if(n==1){       //base case
+//         cout << "1\n";
+//         return;
+//     }
+
+//     cout << n <<" ";
+//     printNums(n-1);
+
+// }
+
+//factorial using recursion
+int factorial(int n){
+    if(n==0){
+        return 1;
     }
+    return n*factorial(n-1);
+}
 
-    cout << n <<" ";
-    printNums(n-1);
 
+
+int main(){
+    //printNums(5);
+    cout << factorial(5);
 }
