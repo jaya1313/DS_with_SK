8a0375e407d9a3484171be8c49858f070cc98930
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-25 15:31:23 +0530

    Ques on recursion

--- lect42/recursion2.cpp
+++ lect42/recursion2.cpp
@@ -2,8 +2,17 @@
 #include<vector>
 using namespace std;
 
-//fabonacci series 
+//fabonacci series using recursion    (509 on leetcode)
+
+int fab(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    return fab(n-1) + fab(n-2);
+}
+
 
 int main(){
+    cout << fab(8);
     return 0;
 }
