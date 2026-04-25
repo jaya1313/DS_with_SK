c198e8c6199b47d8ddbefacf06164e20c0283497
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-25 20:16:12 +0530

    Ques on recursion

--- lect42/recursion2.cpp
+++ lect42/recursion2.cpp
@@ -20,9 +20,28 @@ bool isSorted(vector<int> arr, int n){   // time and space compl. =  O(n)
     return arr[n-1] >= arr[n-2] && isSorted(arr, n-1);
 }
 
+int bSearch(vector<int> arr,int tar,int st,int end){
+    if(st <= end){
+        int mid = st + (end-st) / 2;
+        if(arr[mid] == tar)
+        return mid;
+
+        else if(arr[mid] < tar){
+            return bSearch(arr,tar,mid+1,end);
+        }
+        else{
+            return bSearch(arr,tar,st,mid-1);
+        }
+    }
+    return -1;
+}
+
 int main(){
     vector<int> arr ={1,2,3,4,5};
-    cout << isSorted(arr, arr.size());
+    int end=arr.size()-1;
+    int tar=5;
+    cout << bSearch(arr,tar,0,end);
+    //cout << isSorted(arr, arr.size());
     //cout << fab(8);
     return 0;
 }
