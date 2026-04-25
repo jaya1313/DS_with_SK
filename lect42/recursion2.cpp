c960c784ba4d1b96d15972febc2dcd8652d50bdc
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-25 18:22:57 +0530

    Ques on recursion

--- lect42/recursion2.cpp
+++ lect42/recursion2.cpp
@@ -11,8 +11,18 @@ int fab(int n){
     return fab(n-1) + fab(n-2);
 }
 
+// if array is sorted using recursion
+
+bool isSorted(vector<int> arr, int n){
+    if(n==0 || n==1){
+        return true;
+    }
+    return arr[n-1] >= arr[n-2] && isSorted(arr, n-1);
+}
 
 int main(){
-    cout << fab(8);
+    vector<int> arr ={1,2,3,4,5};
+    cout << isSorted(arr, arr.size());
+    //cout << fab(8);
     return 0;
 }
