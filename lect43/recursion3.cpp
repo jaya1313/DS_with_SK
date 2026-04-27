387d955739757aa1ecefce36dc4c86e33aa8473c
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-27 23:39:52 +0530

    subsets of an array using recursion

--- lect43/recursion3.cpp
+++ lect43/recursion3.cpp
@@ -2,6 +2,25 @@
 #include<vector>
 using namespace std;
 
+//backtracking with recursion
+// find all subsets of an array
+
+int printSubsets(vector<int> arr,vector<int> ans, int i){
+    if(i == arr.size()){
+        //print ans
+        return;
+    }
+    ans.push_back(arr[i]);
+    printSubsets(arr, ans, i+1);
+
+    ans.pop_back();
+    printSubsets(arr, ans, i+1);
+
+}
+
 int main(){
+    vector<int> arr ={1,2,3};
+    vector<int> ans;
+    printSubsets(arr, ans, 0);
     return 0;
 }
