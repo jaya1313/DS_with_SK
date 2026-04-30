5d44c5a7bcd7a014bc6d08ad480b1331bb6257e1
Jaya Karwani <jayakarwani4@gmail.com>
2026-04-30 12:30:59 +0530

    permutation of array using recursion

--- lect44/permutation.cpp
+++ lect44/permutation.cpp
@@ -2,7 +2,21 @@
 #include<vector>
 using namespace std;
 
+// find permutations in array
+void getPermutation(vector<int> &nums, int idx,vector<vector<int>> &ans){
+    //base case
+    if(idx == nums.size()){
+        ans.push_back({nums});
+        return;
+    }
+    for(int i=idx; i<nums.size();i++){
+        swap(nums[idx], nums[i]);   // idx place => ith element choice
+        getPermutation(nums,idx+1,ans);
 
+        swap(nums[idx],nums[i]);  //backtrack
+    }  
+    
+}
 
 int main(){
     vector<int> nums ={1,2,3};
