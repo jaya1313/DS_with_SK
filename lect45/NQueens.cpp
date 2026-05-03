19a59aeee5996d23c73ae645550cf37e09a5d06b
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-03 22:30:32 +0530

    N Queens problem of recursion

--- lect45/NQueens.cpp
+++ lect45/NQueens.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 
-// N Queens problem
+// N Queens problem   51 on leetcode
+
+void nQueens(vector<string> &board, int row, int n, vector<vector<string>> &ans){
+        if(row == n){
+            ans.push_back({board});
+            return;
+        }
+        for(int j=0; j<n; j++){
+            //if(isSafe(board, row, j, n)){
+                board[row][j] = 'Q';
+                nQueens(board, row+1,n,ans);
+                board[row][j] = '.';
+            }
+        }
+    //}
 
 int main(){
     return 0;
