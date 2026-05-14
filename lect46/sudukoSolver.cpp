6a59617416cf1988a0c82f431b3cf55420cbcabd
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-14 20:48:35 +0530

    suduko solver

--- lect46/sudukoSolver.cpp
+++ lect46/sudukoSolver.cpp
@@ -7,9 +7,23 @@ bool sudukoSolver(vector<vector<char>> &board, int row, int col){
         return true;
     }
     
-    
+    for(int dig=1; dig<=9; dig++){
+        if(board[row][col] != '.'){
+            int nextRow; 
+            int nextCol;
+            sudukoSolver(board, nextRow , nextCol);
+        }
+        if(isSafe(board,row,col,dig)){
+            board[row][col] = dig;
+            if(sudukoSolver(board, nextRow , nextCol)){
+                return true;
+            }
+              board[row][col] = '.';
+        }
+    }
 }
 
 int main(){
     return 0;
+
 }
