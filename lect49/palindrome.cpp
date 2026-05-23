7a02f24d61b89ddaaeb579e45ff04436811942f5
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-23 14:20:47 +0530

    palindrome partioning, backtracking problem

--- lect49/palindrome.cpp
+++ lect49/palindrome.cpp
@@ -1,11 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 // palindrome partioning
 //131 on leetcode
 
 
-int main(){
+void getAllPartition(string s, vector<string> &partition, vector<vector<string>> &ans){
+    //base case
+    if(s.size() == 0){
+        ans.push_back(partition);
+        return;
+        }
 
+    for(int i=0; i<s.size(); i++){
+        string part = s.substr(0,i+1);
+        //if(isPalin(part)){
+            partition.push_back(part);
+            getAllPartition(s.substr(i+1), partition, ans);
+            //backtrack
+            partition.pop_back();
+        }
+    }    
+//}
+
+vector<vector<string>> partition(string s){
+    vector<string> partition;
+    vector<vector<string>> ans;
+    getAllPartition(s, partition, ans);
+    return ans;
 }
+
+int main(){
+    string s = "aab";
+    partition(s);
+    //for(string s1 : ans2){
+        //cout << s1 << endl;
+    }
+
+//}
