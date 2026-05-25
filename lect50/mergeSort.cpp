73b47b3c16a2f406b0fa8e41d53465ab0e6c1dbb
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-25 22:48:37 +0530

    merge sort

--- lect50/mergeSort.cpp
+++ lect50/mergeSort.cpp
@@ -2,11 +2,49 @@
 #include<vector>
 using namespace std;
 
+
+void merge(vector<int> &arr,int st, int mid, int end){
+    int i=st;
+    int j = mid+1;
+    vector<int> temp;
+    while(i<=mid && j<=end){
+        if(arr[i] <= arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    //left
+        while (i<=mid){
+            temp.push_back(arr[i]);
+            i++;
+        }
+    //right
+       while(j<=end){
+         temp.push_back(arr[j]);
+         j++;
+       }   
+
+        
+}
+
 void mergeSort(vector<int> &arr, int st, int end){
+    if(st < end){
+        int mid = st + (end-st)/2;
+        mergeSort(arr,st,mid);
+        mergeSort(arr,mid+1,end);
+        merge(arr,st,mid,end);
+        return;
+    }
     
 }
 
 int main(){
 
+    vector<int> arr = {12,8,32,31,54,35};
+    mergeSort(arr,0,arr.size()-1);
     return 0;
 }
