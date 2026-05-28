c5fe9b336d1fce6d9b29a8242d819ac8b0c844b9
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-28 11:43:26 +0530

    Quick Sort

--- lect51/QuickSort.cpp
+++ lect51/QuickSort.cpp
@@ -4,7 +4,17 @@
 using namespace std;
 
 int partition(vector<int> &arr, int st, int end){
-    
+    int idx= st-1;
+    int pivot = arr[end];
+    for(int j=st; j<end; j++){
+        if(arr[j] <= pivot){
+            idx++;
+            swap(arr[j], arr[idx]);
+        }
+    }
+    idx++;
+    swap(arr[end] , arr[idx]);
+    return idx;
 }
 
 void quickSort(vector<int> &arr, int st, int end){
@@ -12,10 +22,15 @@ void quickSort(vector<int> &arr, int st, int end){
         int pivotIdx = partition(arr, st, end);
         quickSort(arr, st, pivotIdx-1);
         quickSort(arr, pivotIdx+1, end);
-        return;
     }
 }
 
 int main(){
+    vector<int> arr ={4,2,6,5,1,3};
+    quickSort(arr, 0, arr.size()-1);
+    for(int val: arr){
+        cout<< val << " ";
+    }
+    cout << endl;
     return 0;
 }
