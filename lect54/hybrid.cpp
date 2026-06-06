657139a66498d7cd4a794bd39abdb4d9d56db421
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-06 18:29:16 +0530

    oops

--- lect54/hybrid.cpp
+++ lect54/hybrid.cpp
@@ -1,23 +1,34 @@
 #include<iostream>
 using namespace std;
 
-//hierarchial inheritance
+//multiple inheritance
 class Person{
     public:
     string name;
     int age;
 };
 
-class Student : public Person{
+class Student:{
     public:
     int rollno;
 };
 
-class GradStudent : public Person{
+class GradStudent : public Person, public Student{
     public:
     string researchArea;
 };
- 
+
+int main(){
+    GradStudent g1;
+    g1.name="rahul";
+    g1.age = 56;
+    g1.rollno = 345;
+    g1.researchArea = "Quantum Physics";
+    cout << g1.name << endl;
+    cout << g1.researchArea << endl;
+    return 0;
+}
+
 int main(){
     GradStudent g1;
     g1.name="rahul";
