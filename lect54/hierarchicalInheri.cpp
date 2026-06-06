49dad5215b45d979669c59606e8c72a71f81d0d2
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-06 18:23:36 +0530

    oops

---
+++ lect54/hierarchicalInheri.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+//multiple inheritance
+class Person{
+    public:
+    string name;
+    int age;
+};
+
+class Student : public Person{
+    public:
+    int rollno;
+};
+
+class GradStudent : public Person{
+    public:
+    string researchArea;
+};
+ 
+int main(){
+    GradStudent g1;
+    g1.name="rahul";
+    g1.age = 56;
+    //g1.rollno = 345;
+    g1.researchArea = "Quantum Physics";
+    cout << g1.name << endl;
+    cout << g1.researchArea << endl;
+    return 0;
+}
