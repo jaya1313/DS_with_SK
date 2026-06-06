1cd9ec0c528486375b99d210f3e19346b664749b
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-07 01:53:29 +0530

    oops

--- lect54/polymorphism2.cpp
+++ lect54/polymorphism2.cpp
@@ -3,12 +3,42 @@ using namespace std;
 
 // Run time polymorphism
 
-//function overriding
+// //function overriding
+
+// class Parent{
+//     public:
+//     void getInfo(){
+//         cout << "parent class" << endl;
+//     }
+// } ;
+
+// class Child : public Parent{
+
+//     public:
+//     void getInfo(){
+//         cout << "child class" << endl;
+//     }
+// };
+
+// int main(){
+//     Child c1;
+//     c1.getInfo();
+//     Parent p1;
+//     p1.getInfo();
+//     return 0;
+// }
+
+
+// 2. virtual function
 
 class Parent{
     public:
     void getInfo(){
         cout << "parent class" << endl;
+    }
+
+    virtual void hello(){
+        cout << "hello from parent" << endl;
     }
 } ;
 
@@ -18,12 +48,16 @@ class Child : public Parent{
     void getInfo(){
         cout << "child class" << endl;
     }
+
+     void hello(){
+        cout << "hello from child" << endl;;
+    }
 };
 
 int main(){
     Child c1;
-    c1.getInfo();
+    c1.hello();
     Parent p1;
-    p1.getInfo();
+    p1.hello();
     return 0;
 }
