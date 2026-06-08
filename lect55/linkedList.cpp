16127b85c5eaef46d1c7957bfecef6726c741adf
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-09 02:05:35 +0530

    linked list

--- lect55/linkedList.cpp
+++ lect55/linkedList.cpp
@@ -22,7 +22,6 @@ class List{
     List(){
         head = tail = NULL;
     }
-    Node* temp , *ttemp;
 
     //push-front
     void push_front(int val){
@@ -33,22 +32,40 @@ class List{
         }
         else{
            newNode->next = head;
-           newNode = head;
+           head = newNode;
         }
     }
 
     void print_all(){
-        temp = head;
-        while(temp->next != NULL){
-            cout << temp->data << " ";
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp->data << " "; // 3->2->1->null
             temp = temp->next;
         }
-        return;
+        cout << endl;
+    }
+
+    // push-back
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            newNode->data = val;
+            newNode->next = NULL;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+
     }
 };
 
 int main(){
     List l1;
     l1.push_front(1);
+    l1.push_front(2);
+    l1.push_front(3);
+    l1.push_back(4);
+    l1.push_back(5);
     l1.print_all();
 }
