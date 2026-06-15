dcb18ce2d1e831e06679a21258cff6e07a928265
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-15 23:52:05 +0530

    merge 2 sorted list

--- lect59/mergeList.cpp
+++ lect59/mergeList.cpp
@@ -43,4 +43,17 @@ class List{
         }
         cout << endl;
     }
+
+    Node* merge2Lists(Node* head1, Node* head2){
+        if(head1 == NULL || head2 == NULL){
+            return head1 = NULL ? head2 : head1;
+        }
+
+        if(head1-> data <= head2->data){
+            head1->next = merge2Lists(head1->next, head2->next);
+        }
+        else{
+            head2->next = merge2Lists(head1, head2->next);
+        }
+    }
 };
