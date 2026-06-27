f8bf3ed9173a1802e878ec57b5eb7e1a4af0cfa7
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-27 14:59:49 +0530

    swap nodes in pair

--- lect65/swapNodesinPair.cpp
+++ lect65/swapNodesinPair.cpp
@@ -1,6 +1,67 @@
 #include<iostream>
 using namespace std;
 
+// swap nodes in pairs
+// 24 on leetcode
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+    public:
+    List(){
+        head = tail = NULL;
+    }
+    // swap nodes in pair
+    Node* swapNodesPair(Node* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        Node* prev = NULL;
+        Node* first = head;
+        Node* sec = head->next;
+        while(first!= NULL && sec != NULL){
+            Node* third = sec->next;
+            sec->next = first;
+            first-> next = third;
+            if(prev != NULL){
+                prev->next = sec;
+            }
+            else{
+                head = sec;
+            }
+            //update
+            prev = first;
+            first = third;
+            if(third != NULL){
+                sec = third->next;
+            }
+            else{
+                sec = NULL;
+            }
+        }
+        return head;
+    }
+};
+
 int main(){
-    
+    // List ll;
+    // ll.push_back(1);
+    // ll.push_back(2);
+    // ll.push_back(3);
+    // ll.push_back(4);
+    // ll.swapNodesPair();
+
+
 }
