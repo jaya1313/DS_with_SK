0e55a0229dc9961119d9eb1bf8a121f44e9a7f8f
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-30 01:01:34 +0530

    stack using vector and linked list

--- lect66/stack.cpp
+++ lect66/stack.cpp
@@ -4,45 +4,79 @@ using namespace std;
 
 // stack using vectors
 
+// class Stack{
+//      vector<int> v;
+
+// public:
+//    //push function
+//    void push(int val){
+//     v.push_back(val);
+//    }     
+
+//    //pop
+//    void pop(){
+//     v.pop_back();
+//    }
+
+//    //top element
+//    int top(){
+//     return v[v.size()-1];
+//    }
+
+//    //empty
+//    bool empty(){
+//     if(v.size() == 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+//    }
+// };
+
+// int main(){
+//     Stack s;
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+//     while(!s.empty()){
+//         cout << s.top() << " ";
+//         s.pop();
+//     }
+//     cout << endl;
+
+// }
+
+
+// stack using linked list
+#include<list>
+
 class Stack{
-     vector<int> v;
 
-public:
-   //push function
-   void push(int val){
-    v.push_back(val);
-   }     
+    list<int> ll;
+    public:
 
-   //pop
-   void pop(){
-    v.pop_back();
-   }
-
-   //top element
-   int top(){
-    return v[v.size()-1];
-   }
-
-   //empty
-   bool empty(){
-    if(v.size() == 0){
-        return true;
+    //push
+    void push(int val){
+        ll.push_front(val);
     }
-    else{
-        return false;
+
+    //pop
+    void pop(){
+        ll.pop_front();
     }
-   }
+
+    //top/head element
+    int top(){
+        return ll.front();
+    }
+
+    //empty
+    bool empty(){
+        return ll.size() == 0;
+    }
 };
 
 int main(){
-    Stack s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    while(!s.empty()){
-        cout << s.top() << " ";
-        s.pop();
-    }
-    cout << endl;
-
+    
 }
