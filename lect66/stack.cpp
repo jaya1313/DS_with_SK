9add384a4ad2fc299482886e9baf9d048d08273e
Jaya Karwani <jayakarwani4@gmail.com>
2026-06-29 23:43:55 +0530

    stack

--- lect66/stack.cpp
+++ lect66/stack.cpp
@@ -22,4 +22,27 @@ public:
    int top(){
     return v[v.size()-1];
    }
+
+   //empty
+   bool empty(){
+    if(v.size() == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+   }
 };
+
+int main(){
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+}
