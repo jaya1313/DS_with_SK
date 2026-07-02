c5082a592a3f18f22a3fd739f460f9a24c5d6970
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-02 23:58:23 +0530

    Next greater

--- lect69/nextGreater.cpp
+++ lect69/nextGreater.cpp
@@ -3,6 +3,34 @@
 #include<stack>
 using namespace std;
 
+//next greater
+// find the next greater no. for each element
+vector<int> nextGreater(vector<int>& arr){
+    stack<int> s;
+    vector<int> ans;
+    int NG;
+    for(int i = arr.size()-1; i>=0; i++){
+        while(s.size() >0 && s.top() <= arr[i]){
+            s.pop();
+        }
+        if(s.empty()){
+           NG = -1;
+        }
+        else{
+            NG = s.top();
+        }
+        s.push(arr[i]);
+        ans.push_back(NG);
+    }
+    return ans;
+}
+
 int main(){
+    vector<int> arr = {6,8,0,1,3};
+    vector<int> anss =  nextGreater(arr);
+    for(int a: anss){
+        cout << a << " ";
+    }
+    cout << endl;
     return 0;
 }
