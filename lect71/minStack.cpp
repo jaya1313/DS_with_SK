a2a8ea4959e5435224cc0bb318d8e05e0d71bfcd
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-04 13:34:21 +0530

    design min stack

--- lect71/minStack.cpp
+++ lect71/minStack.cpp
@@ -3,7 +3,30 @@
 #include<stack>
 using namespace std;
 
+//normal approach
+stack<pair<int, int>> s;
+void push(int val){
+    if(s.empty()){
+        s.push({val, val});
+    } 
+    else{
+        int minVal = min(val, s.top().second);
+        s.push({val, minVal});
+    }
+}
+
+void pop(){
+    s.pop();
+}
+
+int top(){
+    return s.top().first;
+}
+
+int minVal(){
+    return s.top().second;
+}
 
 int main(){
-    
+
 }
