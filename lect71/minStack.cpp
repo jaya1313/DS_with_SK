53ce32932b94cdc5531247ef902b071a02e3fdfd
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-05 23:50:16 +0530

    design min stack

--- lect71/minStack.cpp
+++ lect71/minStack.cpp
@@ -4,29 +4,34 @@
 using namespace std;
 
 //normal approach
-stack<pair<int, int>> s;
-void push(int val){
-    if(s.empty()){
-        s.push({val, val});
-    } 
-    else{
-        int minVal = min(val, s.top().second);
-        s.push({val, minVal});
-    }
-}
+// stack<pair<int, int>> s;
+// void push(int val){
+//     if(s.empty()){
+//         s.push({val, val});
+//     } 
+//     else{
+//         int minVal = min(val, s.top().second);
+//         s.push({val, minVal});
+//     }
+// }
 
-void pop(){
-    s.pop();
-}
+// void pop(){
+//     s.pop();
+// }
 
-int top(){
-    return s.top().first;
-}
+// int top(){
+//     return s.top().first;
+// }
 
-int minVal(){
-    return s.top().second;
-}
+// int minVal(){
+//     return s.top().second;
+// }
 
-int main(){
+// int main(){
 
-}
+// }
+
+// optimal approach
+// we declare global minValue variable change accordingly to push and pop operations
+
+
