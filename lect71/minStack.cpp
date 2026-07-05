a49f32031db99ac41daff62f1a55f002978d7f8c
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-05 23:59:29 +0530

    design min stack brute & optimal

--- lect71/minStack.cpp
+++ lect71/minStack.cpp
@@ -34,4 +34,33 @@ using namespace std;
 // optimal approach
 // we declare global minValue variable change accordingly to push and pop operations
 
+stack<int> s;
+int minValue;
+int getMin(){
+    return minValue;
+}
 
+void push(int val){
+    if(s.empty()){
+        s.push(val);
+        minValue = val;
+    }
+    if(val < minValue){
+        s.push(2*val - minValue);
+        minValue = val;
+    }
+    else{
+        s.push(val);
+    }
+}
+
+void pop(){
+    if(s.top() < minValue){
+        minValue = 2*minValue - s.top();
+    }
+    s.pop();
+}
+
+int top(){
+    return s.top();
+}
