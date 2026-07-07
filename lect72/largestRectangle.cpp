e01206882b77eeb1c5d7fa2fa3961d930d44c528
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-07 23:16:45 +0530

    largest rectangle in the histogram

---
+++ lect72/largestRectangle.cpp
@@ -0,0 +1,11 @@
+// return largest rectangle in the histogram
+// largest means whose area is largest
+
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int main(){
+
+}
