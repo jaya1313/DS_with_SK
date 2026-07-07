781bc2dd60f1502df02bddd6ad5ca26b447d86c5
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-08 00:01:45 +0530

    largest rectangle in the histogram

--- lect72/largestRectangle.cpp
+++ lect72/largestRectangle.cpp
@@ -7,13 +7,40 @@
 using namespace std;
 
 int largestRectangle(vector<int> &height){
-    vector<int> h;
-    vector<int> w;
+    int n = height.size();
+    vector<int> left(n,0);
+    vector<int> right(n,0);
+    stack<int> s;
+
+    // right smaller
+    for(int i=n-1; i>=0; i--){
+        while(s.size() > 0 && height[s.top()] >= height[i]){
+            s.pop();
+        }
+        right[i] = s.empty() ? -1 : s.top();
+        s.push(i);
+    }
+
+    while(!s.empty()){
+        s.pop();
+    }
+
+    // left smaller
+    for(int i=0; i<n; i++){
+        while(s.size() > 0 && height[s.top()] >= height[i]){
+            s.pop();
+        }
+        left[i] = s.empty() ? -1 : s.top();
+        s.push(i);
+    }
+
     int ans =0;
-    for(int i=0; i<height.size();i++){
-        int currArea = h[i] * w[i];
+    for(int i=0; i<n;i++){
+        int width = right[i] - left[i] -1;
+        int currArea = height[i] * width;
         ans = max(currArea, ans);
     }
+    return ans;
 }
 
 int main(){
