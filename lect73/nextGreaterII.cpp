ac8b940bfb2955a6bd1243402ce1b494d33dea85
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-10 23:18:27 +0530

    next greater II

---
+++ lect73/nextGreaterII.cpp
@@ -0,0 +1,16 @@
+// Same as next greater element lect 69
+// with an optimisation that element is connect to first, means a circular loop is there 
+// so we have to traverse through a loop
+// 503 on leetcode
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> nextGreaterII(vector<int>& arr){
+
+}
+
+int main(){
+    return 0;
+}
