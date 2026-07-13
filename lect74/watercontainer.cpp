a6dee6711c6b0892ad93534ad47c9bb16949ce17
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-14 01:33:02 +0530

    rain water conatiner

--- lect74/watercontainer.cpp
+++ lect74/watercontainer.cpp
@@ -6,14 +6,21 @@ using namespace std;
 // trapping rainwater problem
 //better approach
 int trappingRainwater(vector<int> &height){
-    int ans;
-    vector<int> leftMax;
-    vector<int> rightMax;
-    for(int i=0; i<height.size(); i++){
-        
+    int n = height.size();
+    int ans=0;
+    vector<int> leftMax(n,0);
+    vector<int> rightMax(n,0);
+    leftMax[0] = height[0];
+    rightMax[n-1] = height[n-1];
+    for(int i=1; i<n; i++){
+        leftMax[i] = max(leftMax[i-1], height[i]);
     }
+    for(int i=n-2; i>=0; i--){
+        rightMax[i] = max(rightMax[i+1], height[i]);
+    }
+    for(int i=0; i<n; i++){
+       ans+=(min(leftMax[i], rightMax[i]))-height[i];
+    }
+    return ans;
 
-}
-int main(){
-    return 0;
 }
