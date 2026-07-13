590280a2e5ae53ab75216b27e860c7842c52bcd6
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-13 23:14:57 +0530

    rain water conatiner

--- lect74/watercontainer.cpp
+++ lect74/watercontainer.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 // trapping rainwater problem
+//better approach
+int trappingRainwater(vector<int> &height){
+    int ans;
+    vector<int> leftMax;
+    vector<int> rightMax;
+    for(int i=0; i<height.size(); i++){
+        
+    }
+
+}
 int main(){
     return 0;
 }
