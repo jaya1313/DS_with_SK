f326d880123b81467fb58ef88381a4523e4c5998
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-18 22:22:53 +0530

    implementation of Queue using linked list

---
+++ lect77/queue.cpp
@@ -0,0 +1,10 @@
+// queue data structure
+// push - from rear end (from tail of ll)
+// pop - from front end (from head of ll)
+// front - return front (head of ll)
+#include<iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
