84d8ba340aff1b4021e5172fa2bacbf2c3cf9e6e
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-18 22:33:29 +0530

    implementation of Queue using linked list

--- lect77/queue.cpp
+++ lect77/queue.cpp
@@ -5,6 +5,48 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class Queue{
+    public:
+        Node* head;
+        Node* tail;
+
+        Queue(){
+            head = tail = NULL;
+        }
+
+        void push(int val){
+            Node* newNode = new Node(val);
+            if(empty()){
+               head = tail = newNode;
+            }
+            else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+
+        }
+        void pop(){
+
+        }
+        int front(){
+
+        }
+        bool empty(){
+            return head == NULL;
+        }
+}
+
 int main(){
     return 0;
 }
