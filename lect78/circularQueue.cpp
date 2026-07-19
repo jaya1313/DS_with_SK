9bdb68a19ac45420c512d5ad772c521ff4e37abf
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-20 02:35:06 +0530

    circular queue

--- lect78/circularQueue.cpp
+++ lect78/circularQueue.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 // circular queue
+class CircularQueue {
+    int *arr;
+    int currSize, cap;
+    int f,r;
+    
+    public:
+        CircularQueue(int size){
+            cap = size;
+            arr = new int(cap);
+            f=0;
+            r=-1;
+        }
+        void  push(int data);
+};
 
 int main(){
 
