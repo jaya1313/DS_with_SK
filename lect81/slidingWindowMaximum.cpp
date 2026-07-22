7ee8cee7dfad8f2455d2e9d73fba87173b69a4d3
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-22 22:57:37 +0530

    sliding window maximum

---
+++ lect81/slidingWindowMaximum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+using namespace std;
+
+// finding the maximum element from window size = k
+// 239 on leetcode
+
+int main(){
+    return 0;
+}
