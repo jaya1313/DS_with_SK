e5c4acf123feb758df780c435d4a3b9a5644fa99
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-24 23:37:27 +0530

    Gas Station

--- lect82/gasStation.cpp
+++ lect82/gasStation.cpp
@@ -2,40 +2,22 @@
 #include<vector>
 using namespace std;
 
-// gas ststion , 134 on leetcode
-// int gasStation(vector<int> &gas, vector<int> &cost){
-//     int totalGas =0, totalCost=0;
-//     for(int a: gas){
-//         totalGas+= a;
-//     }
-//     for(int a: cost){
-//         totalCost+= a;
-//     }
+//gas ststion , 134 on leetcode
+int gasStation(vector<int> &gas, vector<int> &cost){
+    int totalGas =0, totalCost=0;
+    for(int a: gas){
+        totalGas+= a;
+    }
+    for(int a: cost){
+        totalCost+= a;
+    }
 
-//     if(totalGas < totalCost){
-//         return -1;
-//     }
-
-//     int start=0, currgas=0;
-//     for(int i=0; i< gas.size(); i++){
-//         currgas += (gas[i] - cost[i]);
-
-//         if(currgas < 0){
-//             start = i+1;
-//             currgas=0;
-//         }
-//     }
-//     return start;
-// }
-int gasStation(vector<int>& gas, vector<int>& cost) {
+    if(totalGas < totalCost){
+        return -1;
+    }
 
     int start=0, currgas=0;
-    int totalGas =0, totalCost=0;
     for(int i=0; i< gas.size(); i++){
-    
-        totalGas+=gas[i];
-        totalCost+=cost[i];
-
         currgas += (gas[i] - cost[i]);
 
         if(currgas < 0){
@@ -43,8 +25,28 @@ int gasStation(vector<int>& gas, vector<int>& cost) {
             currgas=0;
         }
     }
-        return totalGas < totalCost ? -1 : start;
+    return start;
 }
+
+// optimised - in 1 loop
+// int gasStation(vector<int>& gas, vector<int>& cost) {
+
+//     int start=0, currgas=0;
+//     int totalGas =0, totalCost=0;
+//     for(int i=0; i< gas.size(); i++){
+    
+//         totalGas+=gas[i];
+//         totalCost+=cost[i];
+
+//         currgas += (gas[i] - cost[i]);
+
+//         if(currgas < 0){
+//             start = i+1;
+//             currgas=0;
+//         }
+//     }
+//         return totalGas < totalCost ? -1 : start;
+// }
 
 int main(){
     vector<int> gas ={1,2,4,5,9};
