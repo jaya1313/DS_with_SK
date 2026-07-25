22f3607cc0c6b9f60bcdbf4e63c498a6ee38d7f4
Jaya Karwani <jayakarwani4@gmail.com>
2026-07-25 23:26:46 +0530

    binary Tree

---
+++ lect83/binaryTree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main(){
+
+    vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    return 0;
+}
