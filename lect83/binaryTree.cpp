7bbb63d7f19c8f5ddcdfb89cbfd8058924adfb6d
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-02 23:13:07 +0530

    binary Tree

--- lect83/binaryTree.cpp
+++ lect83/binaryTree.cpp
@@ -21,15 +21,23 @@ class Node{
 static int idx = -1;
 Node* buildTree(vector<int> &preorder){
     idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
     Node* root = new Node(preorder[idx]);
     root->left = buildTree(preorder);
     root->right = buildTree(preorder);
 
+    return root;
 
 }
 
 int main(){
 
     vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preOrder);
+    cout << root->data << endl;
+    cout << root->left->data << endl;
+    cout << root->right->data << endl;
     return 0;
 }
