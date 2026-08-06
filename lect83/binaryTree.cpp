3e222911b9073960cbf872e20cd8884b73f1a9d3
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-06 23:56:54 +0530

    binary Tree

--- lect83/binaryTree.cpp
+++ lect83/binaryTree.cpp
@@ -76,9 +76,18 @@ void levelOrder(Node* root){
     queue<Node*> q;
     q.push(root);
     while(q.size() > 0){
-        
-    }
+        Node* curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
 
+    }
+  cout << endl;
 }
 
 
