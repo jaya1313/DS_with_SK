f3beb55964159349fb0ad38592160f5798042774
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-09 18:00:00 +0530

    max height,count nodes,sum of nodes of BT

--- lect84/heightOfTree.cpp
+++ lect84/heightOfTree.cpp
@@ -49,13 +49,25 @@ int count(Node* root){
     return leftCount + rightCount+1; 
 }
 
+//sum of nodes of binary tree
+int sum(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftSum = sum(root->left);
+    int rightSum = sum(root->right);
+    return leftSum + rightSum + root->data;
+
+}
+
 
 int main(){
 
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preorder);
-    cout << height(root);
-    cout << count(root);
+    cout <<"height: " << height(root) << endl;
+    cout << "count:" << count(root) << endl;
+    cout << "sum: " << sum(root) << endl;
     return 0;
 
 }
