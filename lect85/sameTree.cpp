09095922ed84a71c118b38a46a11e8734b56ce09
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-11 14:21:54 +0530

    identical Tree

--- lect85/sameTree.cpp
+++ lect85/sameTree.cpp
@@ -37,13 +37,19 @@ bool isIdentical(Node* p, Node* q){
     }
     bool leftSame = isIdentical(p->left, q->left);
     bool rightSame = isIdentical(p->right, q->right);
+    return leftSame && rightSame && p->data == q->data;
 
 }
 
 int main(){
 
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-    Node* root = buildTree(preorder);
+    idx = 0;
+    Node* p = buildTree(preorder);
+
+    idx = 0;
+    Node* q = buildTree(preorder);
+    cout << isIdentical(p,q);
     return 0;
 
 }
