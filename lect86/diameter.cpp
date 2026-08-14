0f7381677d8be477a8b138f96ccbdab828069c48
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-14 23:54:24 +0530

    finding the diameter of tree

---
+++ lect86/diameter.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+
+int main(){
+    
+}
