3ee4c233b6b3747fd4c426d8984e01fade8a115e
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-15 18:20:10 +0530

    diameter Of Binary Tree

--- lect86/diameter.cpp
+++ lect86/diameter.cpp
@@ -28,28 +28,52 @@ Node* buildTree(vector<int> &preorder){
     return root;
 }
 
- int height(Node* root){
+//  int height(Node* root){
+//         if(root == NULL){
+//             return 0;
+//         }
+
+//         int leftHt = height(root->left);
+//         int rightHt = height(root->right);
+//         return max(leftHt, rightHt) + 1;
+//     }
+
+//     int diameterOfBinaryTree(Node* root) {  //O(n*n)
+//         if(root == NULL){
+//             return 0;
+//         }
+
+//         int leftDia = diameterOfBinaryTree(root->left);
+//         int rightDia = diameterOfBinaryTree(root->right);
+//         int currDia = height(root->left) + height(root->right);
+
+//         return max(currDia, max(rightDia, leftDia));
+        
+//     }
+
+    // optimal approach O(n)
+  
+    int ans = 0;
+    int height(Node* root){
         if(root == NULL){
             return 0;
         }
 
         int leftHt = height(root->left);
         int rightHt = height(root->right);
+
+        ans = max(ans, leftHt + rightHt);
         return max(leftHt, rightHt) + 1;
     }
 
-    int diameterOfBinaryTree(Node* root) {
-        if(root == NULL){
-            return 0;
-        }
+    int diameterOfBinaryTree(Node* root) {  
+       height(root);
 
-        int leftDia = diameterOfBinaryTree(root->left);
-        int rightDia = diameterOfBinaryTree(root->right);
-        int currDia = height(root->left) + height(root->right);
-
-        return max(currDia, max(rightDia, leftDia));
+       return ans;
         
     }
+
+
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
