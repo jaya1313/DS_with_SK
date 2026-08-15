72e927d50ada35d75458849a1a4aa49bdd7ee2fc
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-15 18:15:07 +0530

    diameter Of Binary Tree

--- lect86/diameter.cpp
+++ lect86/diameter.cpp
@@ -28,6 +28,33 @@ Node* buildTree(vector<int> &preorder){
     return root;
 }
 
+ int height(Node* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftHt = height(root->left);
+        int rightHt = height(root->right);
+        return max(leftHt, rightHt) + 1;
+    }
+
+    int diameterOfBinaryTree(Node* root) {
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftDia = diameterOfBinaryTree(root->left);
+        int rightDia = diameterOfBinaryTree(root->right);
+        int currDia = height(root->left) + height(root->right);
+
+        return max(currDia, max(rightDia, leftDia));
+        
+    }
+
 int main(){
-    
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+
+    cout << diameterOfBinaryTree(root);
+
 }
