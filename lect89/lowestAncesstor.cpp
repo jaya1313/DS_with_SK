6bb3d2830678988371037a06ddc87f023cf2f9b3
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-23 01:36:04 +0530

    lowest common Ancesstor

--- lect89/lowestAncesstor.cpp
+++ lect89/lowestAncesstor.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // given the 2 nodes p,q  , returning the lowest common ancesstor of p & q
-
+// 236 on leetcode
 class Node{
     public:
     int data;
@@ -32,11 +32,34 @@ Node* buildTree(vector<int> &preorder){
 }
 
 Node* lowestCommonAncesstor(Node* root, Node* p, Node* q){
+
+    if(root == NULL)
+      return NULL;
+
+    if(root ==p || root==q)
+     return root;
+     
+    Node* leftLCA = lowestCommonAncesstor(root->left,p,q);
+    Node* rightLCA = lowestCommonAncesstor(root->right, p,q);
+    
+    if(leftLCA && rightLCA){
+        return root;
+
+    }
+    else if(leftLCA != NULL)
+      return leftLCA;
+      
+    else{
+       return rightLCA;
+    }  
+    
     return 0;
 }
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preorder);
+    // Node* p = preorder[5];
+    // cout << lowestCommonAncesstor(root,4,5);
     return 0;
 }
