f43ab5139b1e59f17b3493e1305bb7a1f2f800e1
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-22 23:56:35 +0530

    lowest common Ancesstor

--- lect89/lowestAncesstor.cpp
+++ lect89/lowestAncesstor.cpp
@@ -30,3 +30,13 @@ Node* buildTree(vector<int> &preorder){
 
     return root;
 }
+
+Node* lowestCommonAncesstor(Node* root, Node* p, Node* q){
+    return 0;
+}
+
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+    return 0;
+}
