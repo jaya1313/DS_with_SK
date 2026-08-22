718af0cafee59fdf3013fb4eda08ba3d161d30dd
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-22 23:54:28 +0530

    lowest common Ancesstor

---
+++ lect89/lowestAncesstor.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+// given the 2 nodes p,q  , returning the lowest common ancesstor of p & q
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
