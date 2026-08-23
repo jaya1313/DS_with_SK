7ae4cbf87b4a6a7f9e91c16e2ad3743e56bfd779
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-23 23:53:26 +0530

    build tree from inorder and preorder

---
+++ lect90/buildTree.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+// build tree from preorder and inorder sequence
+// 105 on leetcode
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
