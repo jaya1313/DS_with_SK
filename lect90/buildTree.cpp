3729618c131ef6d174823f821c84777304d485ad
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-23 23:59:18 +0530

    build tree from inorder and preorder

--- lect90/buildTree.cpp
+++ lect90/buildTree.cpp
@@ -30,3 +30,12 @@ Node* buildTree(vector<int> &preorder){
 
     return root;
 }
+
+
+
+
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+    return 0;
+}
