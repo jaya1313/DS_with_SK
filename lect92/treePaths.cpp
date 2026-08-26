10a315351d851e6b7517546313fc518d98a7995f
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-26 19:03:38 +0530

    finding binary tree paths

---
+++ lect92/treePaths.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+// finding all paths of the given binary tree in string
+// and these strings (paths) then stored in the vector. 
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
