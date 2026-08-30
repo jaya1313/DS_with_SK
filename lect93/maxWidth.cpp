feb2fe44f2af444be15b4a505200306b40f3a40b
Jaya Karwani <jayakarwani4@gmail.com>
2026-08-30 19:22:50 +0530

    finding maximum width of binary tree

--- lect93/maxWidth.cpp
+++ lect93/maxWidth.cpp
@@ -30,3 +30,32 @@ Node* buildTree(vector<int> &preorder){
 
     return root;
 }
+
+int widthOfBinaryTree(Node* root) {
+        
+        queue<pair<Node*, unsigned long long>> q;
+        q.push({root, 0});
+        int maxWidth = 0;
+
+        while(q.size() > 0){
+            int currLevelSize = q.size();
+            unsigned long long stIdx = q.front().second;
+            unsigned long long endIdx = q.back().second;
+
+            maxWidth = max(maxWidth, (int)(endIdx-stIdx+1));
+
+            for(int i=0; i<currLevelSize; i++){
+             auto curr = q.front();
+                q.pop();
+
+                if(curr.first->left){
+                    q.push({curr.first->left, curr.second*2+1});
+                }
+
+                if(curr.first->right){
+                    q.push({curr.first->right, curr.second*2+2});
+                }
+            }
+        }
+        return maxWidth;
+    }
