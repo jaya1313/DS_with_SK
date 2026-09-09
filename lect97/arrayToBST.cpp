3455c0d53945fb278d8f93d30ce809eca1ad9376
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-09 15:03:27 +0530

    sorted array to Binary search Tree

---
+++ lect97/arrayToBST.cpp
@@ -0,0 +1,52 @@
+// sorted array to binary search tree
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+// Binary Search Tree
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Insertion of nodes in BST
+
+Node* insertion(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val < root->data){
+        root->left = insertion(root->left, val);
+    }
+    else{
+        root->right = insertion(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> &arr){
+    Node* root = NULL;
+
+    for(int val: arr){
+        root = insertion(root, val);
+    }
+    return root;
+}
+
+int main(){
+    vector<int> arr={3,2,1,5,6,4};
+    Node* root = buildBST(arr);
+}
