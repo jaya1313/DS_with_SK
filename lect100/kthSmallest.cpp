3e1912d8a80ab2fd2de29fcdf02b92feb446968e
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-10 18:25:19 +0530

    kth smallest element in BST

---
+++ lect100/kthSmallest.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<climits>
+using namespace std;
+
+// Binary Search Tree
+// Finding minimum distance between the nodes of BST
+//  on leetcode
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+        
+    }
+};
+
+// Insertion of nodes in BST
+
+Node* insertion(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val < root->data){
+        root->left = insertion(root->left, val);
+    }
+    else{
+        root->right = insertion(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> &arr){
+    Node* root = NULL;
+
+    for(int val: arr){
+        root = insertion(root, val);
+    }
+    return root;
+}
