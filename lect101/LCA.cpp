de06136b689aea42453a6a562fbab9e81c789bc5
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-11 14:20:01 +0530

    lowest common ancesstor in BST

--- lect101/LCA.cpp
+++ lect101/LCA.cpp
@@ -8,9 +8,6 @@
 #include<climits>
 using namespace std;
 
-// Binary Search Tree
-// Finding kth smallest val in BST
-// 230 on leetcode
 class Node{
     public:
     int data;
@@ -49,4 +46,23 @@ Node* buildBST(vector<int> &arr){
         root = insertion(root, val);
     }
     return root;
+}
+
+Node* LCA(Node* root, Node* p, Node* q){
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(root->data > p->data && root->data > q->data){  //left subtree
+        return LCA(root->left, p, q);
+    }
+
+     if(root->data < p->data && root->data < q->data){  //left subtree
+        return LCA(root->right, p, q);
+    }
+
+    else{
+        return root;
+    }
+
 }
