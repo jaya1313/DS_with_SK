69e7c3d7396586c2bc638579b485012607a1b4cc
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-13 01:57:54 +0530

    Recover BST

--- lect104/recoverBST.cpp
+++ lect104/recoverBST.cpp
@@ -46,12 +46,44 @@ Node* buildBST(vector<int> &arr){
     return root;
 }
 
+    
+
+    void inorder(Node* root, Node* prev, Node* first, Node* second){
+        if(root == NULL){
+            return;
+        }
+
+        inorder(root->left, prev, first, second);
+
+        if(prev != NULL && root->data < prev->data){
+            if(!first){ // first == NULL
+               first = prev;
+            } 
+            second = root;
+        }
+        prev = root;
+        inorder(root->right, prev, first, second);
+    }
+
+    void recoverTree(Node* root) {
+
+        Node* prev = NULL;
+        Node* first = NULL;
+        Node* second = NULL;
+        
+        inorder(root, prev, first, second);
+
+        int temp = first->data;
+        first->data = second->data;
+        second->data = temp;
+    }
+
 int main(){
 
     vector<int> preorder = {1, 3, 8, 6, 4, 9};
     Node* root = buildBST(preorder);
 
-    
+     recoverTree(root);
 
     return 0;
 }
