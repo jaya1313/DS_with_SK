87c4f263767869f918b81a1090753109ba51625f
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-13 01:49:17 +0530

    Recover BST

---
+++ lect104/recoverBST.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<climits>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+        
+    }
+};
+
+Node* insertion(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val < root->data){
+        root->left = insertion(root->left, val);
+    }
+    else{
+        root->right = insertion(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> &arr){
+    Node* root = NULL;
+
+    for(int val: arr){
+        root = insertion(root, val);
+    }
+    return root;
+}
