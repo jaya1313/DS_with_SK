a6328aeb4e64bc19c772c0789c01c0febe4765ee
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-15 18:33:48 +0530

    Inorder predecessor and successor

--- lect108/InOrderPred&Succ.cpp
+++ lect108/InOrderPred&Succ.cpp
@@ -44,3 +44,56 @@ Node* buildBST(vector<int> &arr){
     }
     return root;
 }
+
+Node* rightMostInleft(Node* curr){
+    Node* ans;
+    while(curr != NULL){
+       ans = curr;
+       curr = curr->right;
+    }
+    return ans;
+}
+
+Node* leftMostInRight(Node* curr){
+    Node* ans;
+    while(curr != NULL){
+       ans = curr;
+       curr = curr->left;
+    }
+    return ans;
+}
+
+vector<int> getPredSucc(Node* root, int key){
+    Node* curr = root;
+    Node* pred = NULL;
+    Node* succ = NULL;
+    
+    while(curr != NULL){
+        if(key < curr->data){
+            pred = curr;
+            curr=curr->left;
+        }
+
+        else if(key > curr->data){
+            succ = curr;
+            curr = curr->right;
+        }
+
+        else{
+            if(curr->left != NULL){
+                pred = rightMostInleft(curr->left);
+            }
+
+             if(curr->right != NULL){
+                succ = leftMostInRight(curr->right);
+            }
+            break;
+        }
+    }
+}
+
+
+int main(){
+     vector<int> arr = {6, 4, 1, 5, 8, 7, 9};
+     Node* root = buildBST(arr);
+}
