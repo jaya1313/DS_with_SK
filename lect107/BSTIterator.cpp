d2a81516382318656527a9f7f4efead8090b3465
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-15 13:02:28 +0530

    Binary Search tree Iterator

--- lect107/BSTIterator.cpp
+++ lect107/BSTIterator.cpp
@@ -21,3 +21,32 @@ class Node{
         
     }
 };
+
+Node* insertion(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val < root->data){
+        root->left = insertion(root->left, val);
+    }
+    else{
+        root->right = insertion(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> &arr){
+    Node* root = NULL;
+
+    for(int val: arr){
+        root = insertion(root, val);
+    }
+    return root;
+}
+
+int main() {
+    vector<int> arr = {0, -10, 5, -3, 4, 6};
+    Node* root = buildBST(arr);
+}
