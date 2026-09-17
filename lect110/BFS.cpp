d1c64964def477edff5ab5a9bbdc40a81d76be6d
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-17 12:53:45 +0530

    BFS Traversal

---
+++ lect110/BFS.cpp
@@ -0,0 +1,36 @@
+
+// breadth-first search traversal in graph
+
+#include<iostream>
+#include<list>
+#include<vector>
+using namespace std;
+
+
+class Graph{
+    int V;  // no. of vertices of graph
+    list<int>* l;    // a list 
+
+public:
+    Graph(int V){ //constructor
+       this->V = V;
+       l = new list<int> [V];   // like arr = new int[V]
+    }
+    
+    void addEdge(int u, int v){
+
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void printAdjlist(){
+        for(int i=0; i<V; i++){
+            cout << i << ": ";
+            for(int neighbor : l[i]){
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
+    }
+      
+};
