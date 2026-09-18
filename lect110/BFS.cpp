50b622bca1a84461a69c6d2b6230db5d9969d6cf
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-18 23:06:32 +0530

    BFS Traversal

--- lect110/BFS.cpp
+++ lect110/BFS.cpp
@@ -22,15 +22,19 @@ public:
         l[u].push_back(v);
         l[v].push_back(u);
     }
-
-    void printAdjlist(){
-        for(int i=0; i<V; i++){
-            cout << i << ": ";
-            for(int neighbor : l[i]){
-                cout << neighbor << " ";
-            }
-            cout << endl;
-        }
-    }
       
 };
+
+
+int main(){
+
+    Graph g(5);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+    g.printAdjlist();
+
+    return 0;
+}
