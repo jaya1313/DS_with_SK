4fd4a6f6f670d329faaca59284cabcc9466ee21d
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-22 00:50:17 +0530

    finding number of islands

--- lect114/island.cpp
+++ lect114/island.cpp
@@ -23,6 +23,24 @@ public:
         l[v].push_back(u);
     }
 
+    int island(vector<vector<char>> grid){
+        int islands = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1' && !vis[i][j]) {
+                    dfs(i, j, vis, grid, n, m);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+
     
       
 };
@@ -35,8 +53,6 @@ int main(){
     g.addEdge(0,3);
     g.addEdge(1,2);
     g.addEdge(3,4);
-
-    cout << g.isCycle() << endl;
 
     return 0;
 }
