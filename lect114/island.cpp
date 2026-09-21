f926d9c1722b7a544652c403e0b5b2ca225641a6
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-22 01:14:37 +0530

    finding number of islands

--- lect114/island.cpp
+++ lect114/island.cpp
@@ -7,27 +7,28 @@
 #include<queue>
 using namespace std;
 
-class Graph{
-    int V;  // no. of vertices of graph
-    list<int>* l;    // a list 
+// class Graph{
+//     int V;  // no. of vertices of graph
+//     list<int>* l;    // a list 
 
-public:
-    Graph(int V){ //constructor
-       this->V = V;
-       l = new list<int> [V];   // like arr = new int[V]
-    }
+// public:
+//     Graph(int V){ //constructor
+//        this->V = V;
+//        l = new list<int> [V];   // like arr = new int[V]
+//     }
     
-    void addEdge(int u, int v){
+//     void addEdge(int u, int v){
 
-        l[u].push_back(v);
-        l[v].push_back(u);
-    }
+//         l[u].push_back(v);
+//         l[v].push_back(u);
+//     }
 
     void dfs(int i, int j, vector<vector<bool>> &vis, vector<vector<char>> &grid, int n, int m){
         //base case
-        if(i<0 || j<0 || i>=n || j>=n || grid[i][j] != '1' || vis[i][j]){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != '1' || vis[i][j]){
             return;
         }
+        vis[i][j] = true;
 
         dfs(i-1, j, vis, grid, n, m);
         dfs(i+1, j, vis, grid, n, m);
@@ -55,16 +56,25 @@ public:
 
     
       
-};
+// };
 
 int main(){
 
-    Graph g(5);
-    g.addEdge(0,1);
-    //g.addEdge(0,2);
-    g.addEdge(0,3);
-    g.addEdge(1,2);
-    g.addEdge(3,4);
+    // Graph g(5);
+    // g.addEdge(0,1);
+    // //g.addEdge(0,2);
+    // g.addEdge(0,3);
+    // g.addEdge(1,2);
+    // g.addEdge(3,4);
+
+    vector<vector<char>> grid = {
+    {'1', '1', '0', '0', '0'},
+    {'1', '1', '0', '0', '0'},
+    {'0', '0', '1', '0', '0'},
+    {'0', '0', '0', '1', '1'}
+};
+
+     cout << island(grid) << endl;
 
     return 0;
 }
