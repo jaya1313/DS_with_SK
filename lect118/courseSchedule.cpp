ff9eb1c2366451ca6193fdd7db3f2e28dd51f102
Jaya Karwani <jayakarwani4@gmail.com>
2026-09-25 14:42:32 +0530

    course schedule

---
+++ lect118/courseSchedule.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<list>
+#include<vector>
+#include<stack>
+using namespace std;
+
+// course schedule 207 on leetcode
+
+
+class Graph{
+    int V;  // no. of vertices of graph
+    list<int>* l;    // a list 
+
+public:
+    Graph(int V){ //constructor
+       this->V = V;
+       l = new list<int> [V];   // like arr = new int[V]
+    }
+    
+    void addEdge(int u, int v){
+
+        l[u].push_back(v);
+       
+    }
+
+    void dfs(int curr, vector<bool> &vis, stack<int> &s){  //O(V+E)
+
+        vis[curr] = true;
+
+        for(int v: l[curr]){
+            if(!vis[v]){
+                dfs(v, vis, s);
+            }
+        }
+        s.push(curr);
+    }
+
+   void topoSort(){
+    vector<bool> vis(V, false);
+    stack<int> s;
+
+    for(int i=0; i<V; i++){
+        if(!vis[i]){
+            dfs(i, vis, s);
+        }
+    }
+
+    while(s.size() > 0){
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+   }
+      
+};
+
+int main(){
+
+    Graph g(6);
+
+    g.addEdge(3,1);
+    g.addEdge(2,3);
+    g.addEdge(4,0);
+    g.addEdge(4,1);
+    g.addEdge(5,0);
+    g.addEdge(5,3);
+   
+    g.topoSort();
+
+    return 0;
+}
